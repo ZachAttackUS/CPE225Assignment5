a42ffc2aeafd61e15c7824af2160bd12e295f69f
Zach Mattes <zachmattes@gmail.com>
2023-05-03 18:06:30 -0700

    commit

--- printsort.c
+++ printsort.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
+
+int main(){
 char input;
-printf("Enter word: %c", input);
+printf("Enter word: ");
+readstring()
+
+
+}
+
+void readstring(char str[]){
+    char c = getchar();
+    char mystring[50];
+    int arr[50];
+
+}
