a06366d848cdb837dd0a32db08da67fa74504ef0
Zach Mattes <zachmattes@gmail.com>
2023-05-06 19:17:23 -0700

    finished c excpet stupid %

--- printsort.c
+++ printsort.c
@@ -1,43 +1,77 @@
 #include <stdio.h>
 
-void readstring(char str[20]){
-    int i; char c;
-    while ((c == getchar()) != '\n' && i<19){
+// void readstring(char str[20]){
+//     int i = 0; char c = '\0';
+//     while ((c == getchar()) != '\n' && i<19){
+//         str[i] = c;
+//         i++;
+//     }
+
+//     str[i] ='\0';
+
+// }
+
+void readstring(char str[]) {
+    int i = 0;
+    char c;
+    
+    // Read characters from input until newline or end of array is reached
+    while (i < 19 && (c = getchar()) != '\n') {
         str[i] = c;
         i++;
     }
-
-    str[i] ='\0';
-
-}
-
-void sort_function(char str[20]){
-int x = 0; int length = 0; int i = 0; int k = 0; 
-char lowest; int repeats = 0; char secondlowest;
-while (str[x] != '\0'){
-    length++; //Sets length to be the right length
-    x++;
-}
-x = 0;
-for (i = 0; i < length - 1; i++){
     
-    for (k = i; k < length; k++){
-        if (str[k] < str[lowest]){
-            secondlowest = lowest;
-            lowest = k;
-        if (str[k] == str[lowest]){
-            repeats++;
-        }
-        }
-        
-        
-    }
-    char value = str[lowest];
-    for (x = 0; x < repeats; x++){
-        printf(" %c", value);
-    }
-    lowest = secondlowest;
+    // Add null terminator to end of string
+    str[i] = '\0';
 }
+
+//count letter function to see how many times to print
+int count_letters(char letter, char str[]){
+    int length = 0; int count = 0;
+    while (str[length] != '\0'){
+        length++; //Sets length to be the right length
+
+    }
+    for (int i = 0; i < length; i++){
+        if (letter == str[i]){
+            count++;
+        }
+    
+    }
+    return count;
+}
+
+
+
+
+void sort_function(char str[]){
+    int x = 0; int length = 0; int i = 0; int k = 0; int count;
+    char lowest; int repeats = 0; char secondlowest; char used;
+    while (str[length] != '\0'){
+        length++; //Sets length to be the right length
+
+    }
+
+    used = '0';
+    for (i = 0; i < length; i++){
+        lowest = '{';
+        for (k = 0; k < length; k++){
+            if ((used < str[k]) && (str[k] < lowest)){
+                lowest = str[k];
+
+            }
+            
+        }
+
+        char value = lowest;
+        count = count_letters(value, str);
+        for (int i = 0; i < count; i++){
+            printf("%c", value);
+        }
+
+        used = lowest;
+    }
+
 }
 
 
@@ -47,8 +81,11 @@ for (i = 0; i < length - 1; i++){
 int main(void){
 char str[20];
 printf("Enter word: ");
-readstring(str[20]);
-sort_function(str[20]);
+readstring(str);
+printf("Original word: %s\n" , str);
+printf("Alphabetized word: ");
+sort_function(str);
+
 
 return 0;
 }
